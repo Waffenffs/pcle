b377e79e34c9614d6e3b0b5d84e19f1d329dfeca
Waffen Sultan <waffen.sultanofc@gmail.com>
2024-05-08 20:19:24 +0800

    Fixes #2

--- src/components/Scrollbar.tsx
+++ src/components/Scrollbar.tsx
@@ -3,12 +3,14 @@
 import { useState, useEffect } from "react";
 
 export default function Scrollbar() {
-    const [y, setY] = useState(window.scrollY);
+    const [y, setY] = useState(0);
     const [totalY, setTotalY] = useState(0);
     const [scrollbar, setScrollbar] = useState(0);
 
     useEffect(() => {
         if (typeof window !== "undefined") {
+            setY(window.scrollY);
+
             window.addEventListener("scroll", () => setY(window.scrollY));
 
             const element = document.getElementsByTagName("body")[0];
